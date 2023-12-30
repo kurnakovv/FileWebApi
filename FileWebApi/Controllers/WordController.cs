f55babe34dc9c9237923078b75e709d253764e94
kurnakovv <maksk210@gmail.com>
2023-12-30 16:07:42 +0900

    Add empty word/replacement endpoint

---
+++ FileWebApi/Controllers/WordController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileWebApi.Controllers;
+
+[Route("api/word")]
+[ApiController]
+public class WordController : ControllerBase
+{
+    [HttpPut("replacement")]
+    public async Task<IActionResult> Replace(
+        [Required] string matchString,
+        [Required] string newString,
+        [Required, FromForm] IEnumerable<IFormFile> formFiles
+    )
+    {
+        return Ok();
+    }
+}
