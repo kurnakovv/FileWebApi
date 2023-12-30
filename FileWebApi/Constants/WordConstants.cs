836a8acbc21b5a4b15b1847c1580cdb26e797d3e
kurnakovv <maksk210@gmail.com>
2023-12-30 16:18:23 +0900

    Add validation for word/replacement

---
+++ FileWebApi/Constants/WordConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace FileWebApi.Constants;
+
+/// <summary>
+/// Word constants.
+/// </summary>
+public class WordConstants
+{
+    /// <summary>
+    /// Max 5 MB.
+    /// </summary>
+    public const int MAX_LIMIT = 5 * 1024 * 1024;
+}
--- FileWebApi/Controllers/WordController.cs
+++ FileWebApi/Controllers/WordController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FileWebApi.Constants;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Spire.Doc;
 using System.ComponentModel.DataAnnotations;
 using System.IO.Compression;
@@ -16,6 +18,32 @@ public class WordController : ControllerBase
         [Required, FromForm] IEnumerable<IFormFile> formFiles
     )
     {
+        if (!formFiles.Any())
+        {
+            return NotFound("Files not found.");
+        }
+        IEnumerable<IFormFile> invalidFileFormats = formFiles.Where(f => !f.FileName.EndsWith(".docx"));
+        if (invalidFileFormats.Any())
+        {
+            string names = string.Join(", ", invalidFileFormats.Select(x => $"'{x.FileName}'").ToList());
+            return Conflict($"Incorrect file format, can only be used Doc files (ended with \".docx\").\nIncorrect files: {names}");
+        }
+        IEnumerable<IFormFile> invalidFileSize = formFiles.Where(x => x.Length > WordConstants.MAX_LIMIT);
+        if (invalidFileSize.Any())
+        {
+            string names = string.Join(", ", invalidFileSize.Select(x => $"'{x.FileName}'").ToList());
+            return Conflict($"Incorrect file size (max {WordConstants.MAX_LIMIT} MB).\nIncorrect files: {names}");
+        }
+        IEnumerable<string> duplicateFileNames = formFiles
+            .Select(x => x.FileName)
+            .GroupBy(x => x)
+            .Where(x => x.Count() > 1)
+            .Select(x => x.Key);
+        if (duplicateFileNames.Any())
+        {
+            string names = string.Join(", ", duplicateFileNames.ToList());
+            return Conflict($"Duplicate file names.\nIncorrect files: {names}");
+        }
         MemoryStream outZip = new();
         using (ZipArchive zip = new(outZip, ZipArchiveMode.Create, true))
         {
